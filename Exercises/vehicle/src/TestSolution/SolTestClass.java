247f97c2707a33c37e67539baef2bad386865900
Vincent <VG-Development@web.de>
2018-07-11 23:44:08 +0200

    added test template

---
+++ Exercises/vehicle/src/TestSolution/SolTestClass.java
@@ -0,0 +1,28 @@
+package TestSolution;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolTestClass {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+
+
+    @After
+    public void tearDown() {
+
+    }
+
+
+}
